fd3ab77757f0cd6e71f407c7dfc792030c537d25
unknown <anjalikesavarapu@gmail.com>
2022-06-07 16:46:28 +0530

    Added dark mode and light mode functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './index.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import React from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(false)
+  function toggleDarkMode() {
+    setDarkMode(prevMode => !darkMode)
+  }
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Navbar
+        darkMode={darkMode}
+        toggleDarkMode={toggleDarkMode} />
+      <Main
+        darkMode={darkMode} />
     </div>
   );
 }
